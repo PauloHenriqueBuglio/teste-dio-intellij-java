d613ab6a33489aca5bf7b354af6684cf5ac29929
Paulo Buglio <phbuglio@gmail.com>
2022-05-08 01:27:47 -0300

    Criando Class

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,11 +4,49 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
+        Gato gato = new Gato ();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("Os tres porquinhos do mal", 300);
+        System.out.println(livro1);
+
 
     /*    int a = 5;
         int b = 10;
         System.out.println("boa noite! " + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numpag;
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
     }
 }
